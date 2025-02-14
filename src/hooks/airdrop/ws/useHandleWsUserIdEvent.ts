c236f83ec0d926a3bb683835eca353d1374fc768
Shannon Tenner <shanimal@tari.com>
2025-02-14 16:08:14 +0200

    wip: rm listener and fix gem update in new flow

--- src/hooks/airdrop/ws/useHandleWsUserIdEvent.ts
+++ src/hooks/airdrop/ws/useHandleWsUserIdEvent.ts
@@ -1,50 +1,51 @@
 import { useShellOfSecretsStore } from '@app/store/useShellOfSecretsStore';
 import { WebsocketEventNames, WebsocketUserEvent } from '@app/types/ws';
 import { setFlareAnimationType, setUserGems } from '@app/store';
+import { useCallback } from 'react';
 
 const setTotalBonusTimeMs = useShellOfSecretsStore.getState().setTotalBonusTimeMs;
 const referrals = useShellOfSecretsStore.getState().referrals;
 const setReferrals = useShellOfSecretsStore.getState().setReferrals;
 
-export const handleWsUserIdEvent = (event: string) => {
-    console.debug(`handshake event= `, event);
-    const eventParsed = JSON.parse(event) as WebsocketUserEvent;
-    console.debug(`eventParsed= `, eventParsed);
-    switch (eventParsed.name) {
-        case WebsocketEventNames.REFERRAL_INSTALL_REWARD:
-            setFlareAnimationType('FriendAccepted');
-            break;
-        case WebsocketEventNames.COMPLETED_QUEST:
-            if (eventParsed.data.userPoints?.gems) {
-                setUserGems(eventParsed.data.userPoints?.gems);
+export function useHandleWsUserIdEvent() {
+    return useCallback((event: string) => {
+        const eventParsed = JSON.parse(event) as WebsocketUserEvent;
+        switch (eventParsed.name) {
+            case WebsocketEventNames.REFERRAL_INSTALL_REWARD:
+                setFlareAnimationType('FriendAccepted');
+                break;
+            case WebsocketEventNames.COMPLETED_QUEST:
+                if (eventParsed.data.userPoints?.gems) {
+                    setUserGems(eventParsed.data.userPoints?.gems);
+                }
+                break;
+            case WebsocketEventNames.MINING_STATUS_CREW_UPDATE: {
+                setTotalBonusTimeMs(eventParsed.data.totalTimeBonusMs);
+                break;
             }
-            break;
-        case WebsocketEventNames.MINING_STATUS_CREW_UPDATE: {
-            setTotalBonusTimeMs(eventParsed.data.totalTimeBonusMs);
-            break;
+
+            case WebsocketEventNames.MINING_STATUS_CREW_DISCONNECTED:
+                if (referrals?.activeReferrals) {
+                    const totalActiveReferrals = (referrals?.totalActiveReferrals || 1) - 1;
+                    const referralsUpdated = referrals?.activeReferrals.map((x) => {
+                        if (x.id === eventParsed.data.crewMemberId) {
+                            return { ...x, active: false };
+                        }
+                        return x;
+                    });
+
+                    setReferrals({
+                        ...referrals,
+                        totalActiveReferrals,
+                        activeReferrals: referralsUpdated,
+                    });
+                }
+                break;
+            case WebsocketEventNames.MINING_STATUS_USER_UPDATE:
+                setTotalBonusTimeMs(eventParsed.data.totalTimeBonusMs);
+                break;
+            default:
+                console.warn('Unknown event', eventParsed);
         }
-
-        case WebsocketEventNames.MINING_STATUS_CREW_DISCONNECTED:
-            if (referrals?.activeReferrals) {
-                const totalActiveReferrals = (referrals?.totalActiveReferrals || 1) - 1;
-                const referralsUpdated = referrals?.activeReferrals.map((x) => {
-                    if (x.id === eventParsed.data.crewMemberId) {
-                        return { ...x, active: false };
-                    }
-                    return x;
-                });
-
-                setReferrals({
-                    ...referrals,
-                    totalActiveReferrals,
-                    activeReferrals: referralsUpdated,
-                });
-            }
-            break;
-        case WebsocketEventNames.MINING_STATUS_USER_UPDATE:
-            setTotalBonusTimeMs(eventParsed.data.totalTimeBonusMs);
-            break;
-        default:
-            console.debug('Unknown event', eventParsed);
-    }
-};
+    }, []);
+}
--- src/hooks/airdrop/ws/useSocket.ts
+++ src/hooks/airdrop/ws/useSocket.ts
@@ -1,19 +1,22 @@
 import { useEffect } from 'react';
 import { useAirdropStore } from '@app/store';
-import { handleWsUserIdEvent } from '@app/hooks/airdrop/ws/useHandleWsUserIdEvent.ts';
+import { useHandleWsUserIdEvent } from '@app/hooks/airdrop/ws/useHandleWsUserIdEvent.ts';
 import { OnDisconnectEventMessage, socket, SUBSCRIBE_EVENT } from '@app/utils/socket.ts';
 
 const AUTH_EVENT = 'auth';
 const userId = useAirdropStore.getState().userDetails?.user?.id;
-const airdropToken = useAirdropStore.getState().airdropTokens?.token;
 
 export default function useSocketEvents() {
+    const airdropToken = useAirdropStore((s) => s.airdropTokens?.token);
+    const handleWsUserIdEvent = useHandleWsUserIdEvent();
+
     useEffect(() => {
         socket?.connect();
         return () => {
             socket?.disconnect();
         };
     }, []);
+
     useEffect(() => {
         if (!socket) return;
         function onConnectError(error: Error) {
@@ -32,17 +35,18 @@ export default function useSocketEvents() {
     }, []);
 
     useEffect(() => {
-        console.debug('socket', socket);
         if (!socket) return;
 
-        socket?.emit(SUBSCRIBE_EVENT);
-        socket?.on('connect', () => {
-            console.debug('Connected', userId, !!airdropToken, socket);
+        const onConnect = () => {
             socket?.emit(AUTH_EVENT, airdropToken);
             socket?.on(userId as string, handleWsUserIdEvent);
-        });
-        // return () => {
-        //     socket?.off('connect', onConnect);
-        // };
-    }, []);
+        };
+
+        socket?.emit(SUBSCRIBE_EVENT);
+        socket?.on('connect', onConnect);
+
+        return () => {
+            socket?.off('connect', onConnect);
+        };
+    }, [airdropToken, handleWsUserIdEvent]);
 }
